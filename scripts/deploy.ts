d7c8d2a047ef9ddf66599e2e9fc40b65dfd7ca0b
dperk6 <daniel@perklin.ca>
2021-10-21 00:04:59 -0400

    deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const WrapRegistry = await ethers.getContractFactory("WrapRegistry");
+  const registry = await WrapRegistry.deploy();
 
-  await greeter.deployed();
+  await registry.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("WrapRegistry deployed to:", registry.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
