fa63557ee1712d2e38a80648bf8d3e9575694f66
dperk6 <daniel@perklin.ca>
2021-10-23 23:58:43 -0400

    interfacess

---
+++ contracts/interfaces/IInterestRateModel.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+// QFinance Contracts v2.0.1
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title Compound's JumpRateModel Contract V2
+ * @author Compound (modified by Dharma Labs)
+ * @notice Version 2 modifies Version 1 by enabling updateable parameters.
+ */
+interface IInterestRateModel {
+
+    event NewInterestParams(
+        uint256 baseRatePerBlock,
+        uint256 multiplierPerBlock,
+        uint256 jumpMultiplierPerBlock,
+        uint256 kink1,
+        uint256 kink2,
+        uint256 roof
+    );
+
+    /**
+     * @notice Update the parameters of the interest rate model (only callable by owner, i.e. Timelock)
+     * @param baseRatePerYear The approximate target base APR, as a mantissa (scaled by 1e18)
+     * @param multiplierPerYear The rate of increase in interest rate wrt utilization (scaled by 1e18)
+     * @param jumpMultiplierPerYear The multiplierPerBlock after hitting a specified utilization point
+     * @param kink1_ The utilization point at which the interest rate is fixed
+     * @param kink2_ The utilization point at which the jump multiplier is applied
+     * @param roof_ The utilization point at which the borrow rate is fixed
+     */
+    function updateTripleRateModel(
+        uint256 baseRatePerYear,
+        uint256 multiplierPerYear,
+        uint256 jumpMultiplierPerYear,
+        uint256 kink1_,
+        uint256 kink2_,
+        uint256 roof_
+    ) external;
+
+    /**
+     * @notice Calculates the utilization rate of the market: `borrows / (cash + borrows - reserves)`
+     * @param cash The amount of cash in the market
+     * @param borrows The amount of borrows in the market
+     * @param reserves The amount of reserves in the market (currently unused)
+     * @return The utilization rate as a mantissa between [0, 1e18]
+     */
+    function utilizationRate(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves
+    ) external view returns (uint256);
+
+    /**
+     * @notice Calculates the current borrow rate per block, with the error code expected by the market
+     * @param cash The amount of cash in the market
+     * @param borrows The amount of borrows in the market
+     * @param reserves The amount of reserves in the market
+     * @return The borrow rate percentage per block as a mantissa (scaled by 1e18)
+     */
+    function getBorrowRate(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves
+    ) external view returns (uint256);
+
+    /**
+     * @notice Calculates the current supply rate per block
+     * @param cash The amount of cash in the market
+     * @param borrows The amount of borrows in the market
+     * @param reserves The amount of reserves in the market
+     * @param reserveFactorMantissa The current reserve factor for the market
+     * @return The supply rate percentage per block as a mantissa (scaled by 1e18)
+     */
+    function getSupplyRate(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves,
+        uint256 reserveFactorMantissa
+    ) external view returns (uint256);
+}
