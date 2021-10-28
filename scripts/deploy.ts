2eac646922b063941bfbc077bfa63fe9d7a64965
dperk6 <daniel@perklin.ca>
2021-10-28 09:17:26 -0400

    clean scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+// import { ethers } from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,12 +14,6 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const WrapRegistry = await ethers.getContractFactory("WrapRegistry");
-  const registry = await WrapRegistry.deploy();
-
-  await registry.deployed();
-
-  console.log("WrapRegistry deployed to:", registry.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/index.ts
+++ test/index.ts
@@ -1,38 +1,10 @@
-import { Contract } from "@ethersproject/contracts";
-import { expect } from "chai";
-import { ethers } from "hardhat";
-const abi = require('/Users/dperk6/qfi/qdex/artifacts/contracts/interfaces/IERC20.sol/IERC20.json');
+// import { Contract } from "@ethersproject/contracts";
+// import { expect } from "chai";
+// import { ethers } from "hardhat";
+// const abi = require('/Users/dperk6/qfi/qdex/artifacts/contracts/interfaces/IERC20.sol/IERC20.json');
 
 describe("Check", function () {
 
-  it("Should create a new wrapped token", async function () {
-    const Wrapped = await ethers.getContractFactory("WrapRegistry");
-    const registry = await Wrapped.deploy();
-    await registry.deployed();
-    const newToken = await ethers.getContractFactory("WrappedToken");
-    const token = await newToken.deploy("Test", "TST", registry.address);
-    await token.deployed();
-    expect(await registry.createWrappedToken(token.address));
-  });
-
-  it("Should deposit a token and received wrapped", async function () {
-    const Wrapped = await ethers.getContractFactory("WrapRegistry");
-    const registry = await Wrapped.deploy();
-    await registry.deployed();
-    const newToken = await ethers.getContractFactory("WrappedToken");
-    const token = await newToken.deploy("Test", "TST", registry.address);
-    await token.deployed();
-    await token.approve(registry.address, "1000000000000000000000");
-    const pool = await registry.createWrappedToken(token.address);
-    await pool.wait();
-    const wrappedAddr = await registry.checkToken(token.address);
-    const wrappedContr = await (new Contract(wrappedAddr, abi.abi)).deployed();
-    const baseContr = await (new Contract(token.address, abi.abi)).deployed();
-    const [{ address }] = await ethers.getSigners();
-    /*const balance = await wrappedContr.balanceOf(address).call();
-    const regBalance = await baseContr.balanceOf(registry.address).call();
-    expect(balance).to.equal("1000000000000000000000");
-    expect(regBalance).to.equal("1000000000000000000000");*/
-  });
+  // Add tests here
 
 });
